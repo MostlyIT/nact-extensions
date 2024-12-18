5c8a393ec64696410c91a2a1b24e84c884d0ae37
le0-0 <leonard.schoyen@gmail.com>
2024-12-18 15:57:49 +0100

    Create SnapshotPublisher.ts

---
+++ source/message-handlers/snapshot-publisher/SnapshotPublisher.ts
@@ -0,0 +1,15 @@
+import { combineMessageHandlers } from "../../utility/message-handler/combineMessageHandlers";
+import { createPublishSnapshotMessageHandler } from "./partial-message-handlers/PublishSnapshotMessageHandler";
+import { createSubscribeMessageHandler } from "./partial-message-handlers/SubscribeMessageHandler";
+import { createUnsubscribeMessageHandler } from "./partial-message-handlers/UnsubscribeMessageHandler";
+
+export const createSnapshotPublisher = <TSnapshot>() =>
+  combineMessageHandlers({
+    publishSnapshot: createPublishSnapshotMessageHandler<TSnapshot>(),
+    subscribe: createSubscribeMessageHandler<TSnapshot>(),
+    unsubscribe: createUnsubscribeMessageHandler<TSnapshot>(),
+  });
+
+export type SnapshotPublisher<TSnapshot> = ReturnType<
+  typeof createSnapshotPublisher<TSnapshot>
+>;
