1008032a29a0fad83691fd4fc5525b6acf748084
le0-0 <leonard.schoyen@gmail.com>
2024-12-18 15:52:11 +0100

    Remove old control test.

--- source/control.spec.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from "vitest";
-import { control } from "./control";
-
-test("control", () => {
-  expect(control()).toBe(1);
-});
--- source/control.ts
+++
@@ -1,1 +0,0 @@
-export const control = () => 1;
