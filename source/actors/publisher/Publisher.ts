7b70b40e460332e4c155e23ef57a1959f8670d39
le0-0 <leonard.schoyen@gmail.com>
2025-01-11 14:09:16 +0100

    Publisher: Changed documentation.

--- source/actors/publisher/Publisher.ts
+++ source/actors/publisher/Publisher.ts
@@ -4,7 +4,7 @@ import { PublisherMessage } from "./PublisherMessage";
 declare const publisher: unique symbol;
 
 /**
- * An actor that manages a subscriber list to which it publishes snapshot messages on command.
+ * An actor that manages a subscriber list and publishes snapshot messages sent to it to its subscribers.
  */
 export type Publisher<TSnapshot> = { [publisher]: true } & LocalActorRef<
   PublisherMessage<TSnapshot>
